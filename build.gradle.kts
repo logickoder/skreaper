593e82f58c73c8afd84e0ff8a05989d8143c598e
logickoder <chukwudumebiorazulike@gmail.com>
2023-07-14 15:30:09 +0100

    chore: updated kotlin dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.serialization") version "1.8.22"
+    val kotlin = "1.9.0"
+    kotlin("jvm") version kotlin
+    kotlin("plugin.serialization") version kotlin
     application
 }
 
